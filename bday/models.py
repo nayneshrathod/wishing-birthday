10b64b099722a87daa2a9c048384783fcd5aae12
Naynesh Raghunath Rathod <nayneshrathod@users.noreply.github.com>
2020-08-16 01:35:11 +0530

    make it better

--- bday/models.py
+++ bday/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from django.template.defaultfilters import slugify  # new
+from django.urls import reverse
+
+
+class wish(models.Model):
+    toname = models.CharField(max_length=120)
+    fromname = models.CharField(max_length=120)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return "%s wish to %s" % (self.fromname) (self.toname)
+
+    def get_absolute_url(self):
+
+        return reverse('article_detail', kwargs={'slug': self.__str__()})
+
+    def save(self, *args, **kwargs):  # new
+        if not self.slug:
+            self.slug = slugify(self.toname)
+        return super().save(*args, **kwargs)
--- bday/views.py
+++ bday/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        toname = request.POST["toname"]
+        fromname = request.POST["fromname"]
+        print(toname, fromname)
+        return render(request, 'index.html', {'to': toname, 'from': fromname})
+
     return render(request, 'index.html', {'to': "Shyam", 'from': "Naynesh Rathod"})
