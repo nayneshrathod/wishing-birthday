c67e6fe556ad7f0408179e852efabca0080539f2
Naynesh Raghunath Rathod <48095301+nayneshrathod@users.noreply.github.com>
2020-08-16 16:44:59 +0530

    Update settings.py

--- birthdaywishes/settings.py
+++ birthdaywishes/settings.py
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'birthdaywishes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -92,6 +92,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
