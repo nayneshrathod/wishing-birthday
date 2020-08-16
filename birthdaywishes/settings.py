c0b34af49a7774cef88db228a8420b77f937499b
Naynesh Raghunath Rathod <nayneshrathod@users.noreply.github.com>
2020-08-16 17:24:02 +0530

    save all files url mappimg

--- birthdaywishes/settings.py
+++ birthdaywishes/settings.py
@@ -72,27 +72,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'birthdaywishes.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-'''
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+#
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'd9kt3o7sjecmds',
         'USER': 'pwhylmiulzpsrw',
         'PASSWORD': 'efb28814409e356542711db8c8663969e9f6543f1961b1611f613e58ac87c477',
-        'HOST': 'http://ec2-34-198-243-120.compute-1.amazonaws.com',
+        'HOST': 'ec2-34-198-243-120.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
